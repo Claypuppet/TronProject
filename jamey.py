b262e85ee1476c4d1dc87bcb5c2268f7a1f7c5c8
Rohkin <sir.james.what@gmail.com>
2015-09-15 20:04:18 +0200

    changed some globals

--- jamey.py
+++ jamey.py
@@ -1,39 +1,35 @@
 # The name of the agent. This is a REQUIRED STRING.
 name = 'Jamey'
 
-# You can create variables. They won't be shared or visible by the other agents. They are persistent between matches, not between competitions.
-# Sometimes, you might want to clear them between matches. You'll know a new match has started by reading the info passed through the 'data' parameter.
-previousChoice = None
+import random
+
+globs = {
+	'data': None
+}
 
 # The turn function. This FUNCTION is REQUIRED. It has one passed parameter: the 'data' parameter. To see what's in it, just print the contents to the console.
 # The turn function has a RETURN of type STRING. There are four options: 'NORTH', 'EAST', 'SOUTH' and 'WEST'.
 # Make sure you always return one of these (no typos), as failing to do so will result in a match loss...
 # Also note: if a match hangs/ breaks while the wrapper was working on your turn, you will lose that match!
 def turn(data):
-	# You'll have to 'global' the persistent variables.
-	# Non-persistent vars can be created in function.
-	global previousChoice
-	
-	# When using imports, make sure they'll run on my computer. Please, ask me to do a test-run BEFORE submitting.
-	import random
-	
-	global width = data.world_width_current
-	global height = data.world_height_current
+	global globs;
 
-	global board = world_data
-
-	global player = {
-		'x' = data.agent[0].position.x,
-		'y' = data.agent[0].position.y
-	}
-
-	global enemy = {
-		'x' = data.agent[1].position.x,
-		'y' = data.agent[1].position.y
-	}
-
+	globs['data'] = data;
 
 	
-
-
 	return choice
+
+def fill () :
+	global globs;
+
+
+def viableDirections () :
+	
+
+def detectLock () :
+
+def determineChoice () :
+
+def startingPosition () :
+
+
